5a51504d345e1bbfd79dadb9d9ac1b571b919243
Jonathan Meffert <jlmeffert@yahoo.com>
2025-02-17 01:09:22 -0500

    instruction #2

--- personal-budget/src/app/hero/hero.component.ts
+++ personal-budget/src/app/hero/hero.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'pb-hero',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
